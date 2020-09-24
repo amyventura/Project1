5cfc9f6514ef18fdf05e3a9cd0e41d7b683d8e8a
Amy Ventura <amyvntr@yahoo.com>
2020-09-23 23:49:26 -0400

    final video changes

--- video_page.js
+++ video_page.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var key = "AIzaSyA-2N_iTiqbNcDh3UJu6Jk6-kBZPRAFBag";
+    var key = "AIzaSyB2ovQlkra_1Da0lFRFXlzyVbQyohH1KDU";
     var video = "";
 
 
@@ -16,14 +16,14 @@ $(document).ready(function() {
         $("#videos").empty();
 
         $.ajax({
-            url: "//www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,
+            url: "https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,
             method: "GET",
         }).then(function(response) {
             console.log(response);
             // for (i = 0; i < response.items.length; i++){
             response.items.forEach(item => {
                 video = `
-                    <iframe width="420" height="315" src=//www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowFullScreen></iframe>
+                    <iframe width="420" height="315" src=http://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowFullScreen></iframe>
                 `
                 $("#videos").append(video);
                 }
